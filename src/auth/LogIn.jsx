4596ca738a9def70684e8595661935a5b18640a2
devanshu rana <devanshurana75@gmail.com>
2023-01-28 22:34:18 +0530

    authentication completed

--- src/auth/LogIn.jsx
+++ src/auth/LogIn.jsx
@@ -1,29 +1,93 @@
+import { useState } from "react"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+
 export const LogIn = () => {
+
+    const [formData, SetFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const [passError, SetPassError] = useState("Enter Password");
+    const [emailError, SetemailError] = useState("Enter your email");
+    const [FormError, SetFormError] = useState();
+
+    const handleChange = (event) => {
+        SetFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+        Login();
+    }
+
+    const Login = () => {
+        signInWithEmailAndPassword(auth, formData.email, formData.password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log("login successful");
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+                console.log(errorCode);
+
+                if (errorCode === "auth/invalid-email") {
+                    SetemailError("Enter Valid Email")
+                } else if (errorCode === "auth/invalid-password") {
+                    SetPassError("Password wrong");
+                } else {
+                    SetFormError("Login Fail! Enter a valid credential");
+                }
+
+            });
+    }
+
     return (
         <div className="continaer-fluid h-100">
-        <div className="row h-100">
-            <div className="col-md-3 logo-bg d-flex">
-                <h1>Login</h1>
-            </div>
-            <div className="col-md-9 form-content d-flex justify-content-center align-items-center">
-                <form className="w-50">
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="text" class="form-control" placeholder="example@gmail.com" />
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" placeholder="Enter your password" />
-                    </div>
-                    <div>
-                        <p className="login_text">Forgot Your Password?</p>
-                    </div>
-                    <div className="justify-content-center align-items-center d-flex">
-                        <button type="button" class="btn form_btn">LogIn</button>
-                    </div>
-                </form>
+            <div className="row h-100">
+                <div className="col-md-3 logo-bg d-flex">
+                    <h1>Login</h1>
+                </div>
+                <div className="col-md-9 form-content d-flex justify-content-center align-items-center">
+                    <form onSubmit={handleSubmit} className="w-50">
+                        <div class="mb-3">
+                            <label for="email" className="form-label">Email</label>
+                            <input type="text"
+                                onChange={handleChange}
+                                name="email"
+                                className="form-control"
+                                value={formData.email}
+                                placeholder="example@gmail.com" />
+                            <p>{emailError}</p>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" className="form-label">Password</label>
+                            <input type="password"
+                                onChange={handleChange}
+                                value={formData.password}
+                                name="password"
+                                className="form-control"
+                                placeholder="Enter your password" />
+                            <p>{passError}</p>
+                        </div>
+                        <div>
+                            <p className="login_text">Forgot Your Password?</p>
+                        </div>
+                        <p>{FormError}</p>
+                        <div className="justify-content-center align-items-center d-flex">
+                            <button type="submit" className="btn form_btn">LogIn</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
--- src/auth/Registration.jsx
+++ src/auth/Registration.jsx
@@ -1,4 +1,92 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { app } from "../firebase";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const db = getFirestore(app);
+const auth = getAuth();
+
 export const Registration = () => {
+    const navigate = useNavigate();
+    const [formData, SetFormData] = useState({
+        userName: "",
+        email: "",
+        password: "",
+        confirm_password: ""
+    })
+
+    const [formErrors, SetformErrors] = useState({});
+    const [isValid, SetisValid] = useState("false")
+
+
+    const handleChange = (event) => {
+        SetFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+
+        if (isValid === false) {
+            validate();
+        }
+        else {
+            SetformErrors(validate(formData));
+            SetisValid("true")
+            StoreData();
+            navigate("login")
+            console.log(formData);
+        }
+    }
+
+    const StoreData = () => {
+        console.log("Function Called")
+        createUserWithEmailAndPassword(auth, formData.email, formData.password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                const docRef = doc(db, "users", user.uid);
+                try {
+                    setDoc(docRef, {
+                        userName: formData.userName,
+                        email: formData.email,
+                    })
+                    console.log("Document writtent with ID:", docRef.id);
+                } catch (e) {
+                    console.error("Error adding document:", e);
+                }
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
+
+    const validate = (formData) => {
+        const errors = {}
+
+        if (!formData.userName) {
+            errors.userName = "User Name is required"
+        }
+        if (!formData.email) {
+            errors.email = "Email is required"
+        }
+        if (!formData.password) {
+            errors.password = "Password is require"
+        } else if (formData.password > 4) {
+            errors.password = "Password must be min of 8 character"
+        }
+        if (!formData.confirm_password) {
+            errors.confirm_password = "Confirm password is required"
+        } else if (formData.confirm_password !== formData.password) {
+            errors.confirm_password = "Password must be same "
+        }
+        return errors;
+    }
+
     return (
         <div className="continaer-fluid h-100">
             <div className="row h-100">
@@ -6,25 +94,45 @@ export const Registration = () => {
                     <h1>Registration Form</h1>
                 </div>
                 <div className="col-md-9 form-content d-flex justify-content-center align-items-center">
-                    <form className="w-50">
+                    <form className="w-50" onSubmit={handleSubmit}>
                         <div class="mb-3">
                             <label for="username" class="form-label">UserName</label>
-                            <input type="text" class="form-control" placeholder="Enter UserName" />
+                            <input type="text"
+                                onChange={handleChange}
+                                name="userName"
+                                value={formData.userName}
+                                class="form-control" placeholder="Enter UserName" />
+                            <p>{formErrors.userName}</p>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="text" class="form-control" placeholder="example@gmail.com" />
+                            <input type="text"
+                                onChange={handleChange}
+                                name="email"
+                                value={formData.email}
+                                class="form-control" placeholder="example@gmail.com" />
+                            <p>{formErrors.email}</p>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" placeholder="Password must be 8 Character" />
+                            <input type="password"
+                                onChange={handleChange}
+                                name="password"
+                                value={formData.password}
+                                class="form-control" placeholder="Password must be 8 Character" />
+                            <p>{formErrors.password}</p>
                         </div>
                         <div class="mb-3">
                             <label for="Confirm-password" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" placeholder="Confirm Password" />
+                            <input type="password"
+                                onChange={handleChange}
+                                name="confirm_password"
+                                value={formData.confirm_password}
+                                class="form-control" placeholder="Confirm Password" />
+                            <p>{formErrors.confirm_password}</p>
                         </div>
                         <div className="justify-content-center align-items-center d-flex">
-                            <button type="button" class="btn form_btn">SignUp</button>
+                            <button type="submit" class="btn form_btn">SignUp</button>
                         </div>
                     </form>
                 </div>
